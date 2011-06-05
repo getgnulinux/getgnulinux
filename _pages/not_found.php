42f7586f4259d205b7bfc5f164cb3c2a8c934ebb
Serrano Pereira <figure002@gmail.com>
2011-06-05 16:37:30 +0200

    Link to the bug tracker on the Not Found page now links to Launchpad Bugs. Stylesheets updated so rounded corners work in Google Chrome as well. Hyperlink markup changed; dotted underline removed and now used for acronyms and different colors for hyperlinks. Removed unused footers.

--- _pages/not_found.php
+++ _pages/not_found.php
@@ -4,10 +4,10 @@
 
 <h1><?php print _("Doh! Page Not Found"); ?></h1>
 
-<p><?php print _("Sorry, but we couldn't find the page you were looking for. Either that page doesn't exist or the web page went AWOL."); ?></p>
+<p><?php print _("Sorry, but we couldn't find the page you were looking for. Either that page doesn't exist or the web page went <acronym title=\"away without leave\">AWOL</acronym>."); ?></p>
 
 <h2><?php print _("Report broken link"); ?></h2>
-<p><?php print _("If you think you found a broken link, please <a href=\"/bug_tracker/\">report it</a> so we can fix it. Thank you!"); ?></p>
+<p><?php print _("If you think you found a broken link, please <a href=\"https://bugs.launchpad.net/getgnulinux\">report it</a> so we can fix it. Thank you!"); ?></p>
 
 <br/><a href="<?php base_url(); ?>"><?php print _("Return to the home page"); ?></a>
 
