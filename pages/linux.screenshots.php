25d8855f4f01f139cde61d977914a8e310b95b79
Serrano Pereira <figure002@gmail.com>
2011-06-19 01:51:19 +0200

    Fixed broken screenshot links.

--- pages/linux.screenshots.php
+++ pages/linux.screenshots.php
@@ -12,38 +12,38 @@
 <p><?php print _("Keep in mind that Linux can look as frugal or exciting as you require; here are only everyday-user snapshots to show common tasks."); ?></p>
 
 <div class="even">
-<a href="/_language/screenshots/ubuntu1.png"><img height="150" width="200" src="/images/screenshots/h150px/ubuntu1.png" alt="|| snapshot1 ||"/></a>
+<a href="/images/screenshots/ubuntu1.png"><img height="150" width="200" src="/images/screenshots/h150px/ubuntu1.png" alt="|| snapshot1 ||"/></a>
 <p><em>Ubuntu:</em></p>
 <p><?php print _("Firefox web browser"); ?></p>
 </div>
 
 <div class="even">
-<a href="/_language/screenshots/ubuntu2.png"><img height="150" width="200" src="/images/screenshots/h150px/ubuntu2.png" alt="|| snapshot2 ||"/></a>
+<a href="/images/screenshots/ubuntu2.png"><img height="150" width="200" src="/images/screenshots/h150px/ubuntu2.png" alt="|| snapshot2 ||"/></a>
 <p><em>Ubuntu:</em></p>
 <p><?php print _("File manager and image browing application are open, on standard desktop."); ?></p>
 </div>
 
 <div class="odd">
-<a href="/_language/screenshots/fedora1.png"><img height="150" src="/images/screenshots/h150px/fedora1.png" alt="|| snapshot3 ||"/></a>
+<a href="/images/screenshots/fedora1.png"><img height="150" src="/images/screenshots/h150px/fedora1.png" alt="|| snapshot3 ||"/></a>
 <p><em>Fedora:</em></p>
 <p><?php print _("Gimp image editing application, cropping a photo."); ?></p>
 </div>
 
 <div class="odd">
-<a href="/_language/screenshots/fedora2.png"><img height="150" src="/images/screenshots/h150px/fedora2.png" alt="|| snapshot4 ||"/></a>
+<a href="/images/screenshots/fedora2.png"><img height="150" src="/images/screenshots/h150px/fedora2.png" alt="|| snapshot4 ||"/></a>
 <p><em>Fedora:</em></p>
 <p><?php print _("OpenOffice.org Writer open, editing a standard document."); ?></p>
 </div>
 
 
 <div class="even">
-<a href="/_language/screenshots/gnewsense1.png"><img height="150" src="/images/screenshots/h150px/gnewsense1.png" alt="|| snapshot5 ||"/></a>
+<a href="/images/screenshots/gnewsense1.png"><img height="150" src="/images/screenshots/h150px/gnewsense1.png" alt="|| snapshot5 ||"/></a>
 <p><em>gNewSense:</em></p>
 <p><?php print _("File browser open."); ?></p>
 </div>
 
 <div class="even">
-<a href="/_language/screenshots/gnewsense2.png"><img height="150" src="/images/screenshots/h150px/gnewsense2.png" alt="|| snapshot6 ||"/></a>
+<a href="/images/screenshots/gnewsense2.png"><img height="150" src="/images/screenshots/h150px/gnewsense2.png" alt="|| snapshot6 ||"/></a>
 <p><em>gNewSense:</em></p>
 <p><?php print _("OpenOffice.org Calc open, showing an example spreadsheet."); ?></p>
 </div>
