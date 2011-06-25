43e6eda9d369a7abb17f741b2c139b3e19a37693
Serrano Pereira <figure002@gmail.com>
2011-06-25 15:47:32 +0200

    Merged the two locale arrays in the GetGnuLinux class. Other codes are updated for this change.

--- include/GetGnuLinux.php
+++ include/GetGnuLinux.php
@@ -7,39 +7,22 @@ class GetGnuLinux {
         'dir' => "ltr",
         );
     var $locale_info = array(
-        'ar' => array("الرئيسية",'rtl',"العربية", "احصل على هذه الصفحة باللغة العربية !", "sa"),
-        'ca' => array("Inici",'ltr',"català", "Traduïu aquesta pàgina a la llengua català!", "ad"),
-        'en' => array("Home",'ltr',"English", "Watch this page in English", "gb"),
-        'de' => array("Home",'ltr',"Deutsch", "Watch this page in German", "de"),
-        'es' => array("Inicio",'ltr',"castellano", "¡Lee esta página en castellano!", "es"),
-        'fr' => array("Accueil",'ltr',"français", "Cette page en français", "fr"),
-        'it' => array("Home",'ltr',"italiano", "Watch this page in Italian", "it"),
-        'ml' => array("Home",'ltr',"മലയാളം", "Watch this page in Malayalam", "in"),
-        'nl' => array("Hoofdpagina",'ltr',"Nederlands", "Bekijk deze pagina in het Nederlands", "nl"),
-        'pt' => array("Home",'ltr',"português", "Watch this page in Portuguese", "br"),
-        'ru' => array("Домой",'ltr',"русский", "Просмотреть эту страницу на русский языке!", "ru"),
-        'sr' => array("Home",'ltr',"српски", "Watch this page in Serbian", "rs"),
-        #'uk' => array("Додому",'ltr',"українська", "Переглянути цю сторінку на українська мові!", "ua"),
-        'tr' => array("Home",'ltr',"Türkçe", "Watch this page in Turkish", "tr"),
-        'vi' => array("Nhà",'ltr',"Tiếng Việt", "Xem trang này bằng tiếng Tiếng Việt !", "vn"),
-        'zh' => array("Home",'ltr',"汉语", "Watch this page in Chinese", "cn"),
-        );
-    var $supported_locales = array(
-        'ar' => 'ar_SA.UTF-8',
-        'ca' => 'ca_AD.UTF-8',
-        'de' => 'de_DE.UTF-8',
-        'en' => 'en_GB.UTF-8',
-        'es' => 'es_ES.UTF-8',
-        'fr' => 'fr_FR.UTF-8',
-        'it' => 'it_IT.UTF-8',
-        'ml' => 'ml_IN',
-        'nl' => 'nl_NL.UTF-8',
-        'pt' => 'pt_PT.UTF-8',
-        'ru' => 'ru_RU.UTF-8',
-        'sr' => 'sr_RS',
-        'tr' => 'tr_TR.UTF-8',
-        'vi' => 'vi_VN',
-        'zh' => 'zh_CN.UTF-8',
+        'ar' => array('ar_SA.UTF-8',"الرئيسية",'rtl',"العربية", "احصل على هذه الصفحة باللغة العربية !"),
+        'ca' => array('ca_AD.UTF-8',"Inici",'ltr',"català", "Traduïu aquesta pàgina a la llengua català!"),
+        'en' => array('en_GB.UTF-8',"Home",'ltr',"English", "Watch this page in English"),
+        'de' => array('de_DE.UTF-8',"Home",'ltr',"Deutsch", "Watch this page in German"),
+        'es' => array('es_ES.UTF-8',"Inicio",'ltr',"castellano", "¡Lee esta página en castellano!"),
+        'fr' => array('fr_FR.UTF-8',"Accueil",'ltr',"français", "Cette page en français"),
+        'it' => array('it_IT.UTF-8',"Home",'ltr',"italiano", "Watch this page in Italian"),
+        'ml' => array('ml_IN',"Home",'ltr',"മലയാളം", "Watch this page in Malayalam"),
+        'nl' => array('nl_NL.UTF-8',"Hoofdpagina",'ltr',"Nederlands", "Bekijk deze pagina in het Nederlands"),
+        'pt' => array('pt_PT.UTF-8',"Home",'ltr',"português", "Watch this page in Portuguese"),
+        'ru' => array('ru_RU.UTF-8',"Домой",'ltr',"русский", "Просмотреть эту страницу на русский языке!"),
+        'sr' => array('sr_RS',"Home",'ltr',"српски", "Watch this page in Serbian"),
+        #'uk' => array('uk_UA.UTF-8',"Додому",'ltr',"українська", "Переглянути цю сторінку на українська мові!"),
+        'tr' => array('tr_TR.UTF-8',"Home",'ltr',"Türkçe", "Watch this page in Turkish"),
+        'vi' => array('vi_VN',"Nhà",'ltr',"Tiếng Việt", "Xem trang này bằng tiếng Tiếng Việt !"),
+        'zh' => array('zh_CN.UTF-8',"Home",'ltr',"汉语", "Watch this page in Chinese"),
         );
 
     // constructor
@@ -49,7 +32,7 @@ class GetGnuLinux {
         $this->set('locale', $locale);
 
         # Set the text direction.
-        $dir = $this->locale_info[$this->get('lang')][1];
+        $dir = $this->locale_info[$this->get('lang')][2];
         $this->set('dir', $dir);
 
         # Initialize gettext.
@@ -84,8 +67,8 @@ class GetGnuLinux {
 
     function set_locale($locale)
     {
-        if ( array_key_exists($locale, $this->supported_locales) ) {
-            $this->conf['locale'] = $this->supported_locales[$locale];
+        if ( array_key_exists($locale, $this->locale_info) ) {
+            $this->conf['locale'] = $this->locale_info[$locale][0];
         }
     }
 
@@ -93,6 +76,9 @@ class GetGnuLinux {
     {
         if ($key == 'lang') {
             return substr($this->conf['locale'], 0, 2);
+        }
+        else if ($key == 'country_code') {
+            return strtolower(substr($this->conf['locale'], 3, 2));
         }
 
 		if ( isset($this->conf[$key]) ) {
--- include/header.php
+++ include/header.php
@@ -7,7 +7,7 @@
     <?php
     foreach ($ggl->locale_info as $id => $items) {
         printf("<link rel=\"alternate\" type=\"text/html\" dir=\"%s\" hreflang=\"%s\" href=\"/%s/\" title=\"%s\" />\n",
-            $items[1], $id, $id, $items[3]);
+            $items[2], $id, $id, $items[4]);
     }
     ?>
 
@@ -103,7 +103,7 @@
 <?php
 foreach ($ggl->locale_info as $id => $items) {
     printf("<li%s><a href=\"/%s/%s\" hreflang=\"%s\"><img src=\"/images/flags/%s.gif\" alt=\"%s\" /> <span class=\"language\" dir=\"%s\">%s</span></a></li>\n",
-        $this->is_current_language($id), $id, $this->current_page(), $id, $items[4], $id, $items[1], $items[2]);
+        $this->is_current_language($id), $id, $this->current_page(), $id, strtolower(substr($items[0], 3, 2)), $id, $items[2], $items[3]);
 }
 ?>
 </ul>
