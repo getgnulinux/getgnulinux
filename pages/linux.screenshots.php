9a8977a8b16d70fb2547b10fb98b7ff3e0f78e28
Serrano Pereira <figure002@gmail.com>
2011-07-10 12:06:03 +0200

    Added three more screenshots with description to the screenshots page.

--- pages/linux.screenshots.php
+++ pages/linux.screenshots.php
@@ -8,11 +8,11 @@
 
 <h1><?php print _("What GNU/Linux looks like"); ?></h1>
 
-<p><?php print _("Six selected screenshots of distributions of GNU/Linux."); ?></p>
+<p><?php print _("Selected screenshots of distributions of GNU/Linux."); ?></p>
 <p><?php print _("Keep in mind that Linux can look as frugal or exciting as you require; here are only everyday-user snapshots to show common tasks."); ?></p>
 
 <div class="even">
-<a href="/images/screenshots/ubuntu1.png"><img src="/images/screenshots/h150px/ubuntu1.png" alt="|| snapshot1 ||"/></a>
+<a href="/images/screenshots/ubuntu1.png"><img height="150" src="/images/screenshots/h150px/ubuntu1.png" alt="Ubuntu Snapshot 1"/></a>
 <p><em>Ubuntu:</em></p>
 <p><?php
 # i18n: This is the description of the "ubuntu1.png" screenshot.
@@ -20,32 +20,48 @@ print _("Firefox web browser"); ?></p>
 </div>
 
 <div class="even">
-<a href="/images/screenshots/ubuntu2.png"><img src="/images/screenshots/h150px/ubuntu2.png" alt="|| snapshot2 ||"/></a>
+<a href="/images/screenshots/ubuntu2.png"><img height="150" src="/images/screenshots/h150px/ubuntu2.png" alt="Ubuntu Snapshot 2"/></a>
 <p><em>Ubuntu:</em></p>
 <p><?php
 # i18n: This is the description of the "ubuntu2.png" screenshot.
 print _("File manager and image browing application are open, on standard desktop."); ?></p>
 </div>
 
-<div class="odd">
-<a href="/images/screenshots/fedora1.png"><img height="150" src="/images/screenshots/h150px/fedora1.png" alt="|| snapshot3 ||"/></a>
-<p><em>Fedora:</em></p>
+<div class="even">
+<a href="/images/screenshots/ubuntu3.png"><img height="150" src="/images/screenshots/h150px/ubuntu3.png" alt="Ubuntu Snapshot 3"/></a>
+<p><em>Ubuntu:</em></p>
 <p><?php
-# i18n: This is the description of the "fedora1.png" screenshot.
-print _("Gimp image editing application, cropping a photo."); ?></p>
+# i18n: This is the description of the "ubuntu3.png" screenshot.
+print _("Evolution e-mail and calendar application."); ?></p>
 </div>
 
 <div class="odd">
-<a href="/images/screenshots/fedora2.png"><img height="150" src="/images/screenshots/h150px/fedora2.png" alt="|| snapshot4 ||"/></a>
+<a href="/images/screenshots/fedora1.png"><img height="150" src="/images/screenshots/h150px/fedora1.png" alt="Fedora Snapshot 1"/></a>
+<p><em>Fedora:</em></p>
+<p><?php
+# i18n: This is the description of the "fedora1.png" screenshot.
+print _("GIMP image editing application, cropping a photo."); ?></p>
+</div>
+
+<div class="odd">
+<a href="/images/screenshots/fedora2.png"><img height="150" src="/images/screenshots/h150px/fedora2.png" alt="Fedora Snapshot 2"/></a>
 <p><em>Fedora:</em></p>
 <p><?php
 # i18n: This is the description of the "fedora2.png" screenshot.
 print _("OpenOffice.org Writer open, editing a standard document."); ?></p>
 </div>
 
+<div class="odd">
+<a href="/images/screenshots/fedora3.png"><img height="150" src="/images/screenshots/h150px/fedora3.png" alt="Fedora Snapshot 3"/></a>
+<p><em>Fedora:</em></p>
+<p><?php
+# i18n: This is the description of the "fedora3.png" screenshot.
+print _("A new way of accessing applications in the <a href=\"http://gnome3.org/\">GNOME 3</a> desktop environment."); ?></p>
+</div>
+
 
 <div class="even">
-<a href="/images/screenshots/gnewsense1.png"><img height="150" src="/images/screenshots/h150px/gnewsense1.png" alt="|| snapshot5 ||"/></a>
+<a href="/images/screenshots/gnewsense1.png"><img height="150" src="/images/screenshots/h150px/gnewsense1.png" alt="gNewSense Snapshot 1"/></a>
 <p><em>gNewSense:</em></p>
 <p><?php
 # i18n: This is the description of the "gnewsense1.png" screenshot.
@@ -53,11 +69,19 @@ print _("File browser open."); ?></p>
 </div>
 
 <div class="even">
-<a href="/images/screenshots/gnewsense2.png"><img height="150" src="/images/screenshots/h150px/gnewsense2.png" alt="|| snapshot6 ||"/></a>
+<a href="/images/screenshots/gnewsense2.png"><img height="150" src="/images/screenshots/h150px/gnewsense2.png" alt="gNewSense Snapshot 2"/></a>
 <p><em>gNewSense:</em></p>
 <p><?php
 # i18n: This is the description of the "gnewsense2.png" screenshot.
 print _("OpenOffice.org Calc open, showing an example spreadsheet."); ?></p>
+</div>
+
+<div class="even">
+<a href="/images/screenshots/gnewsense3.png"><img height="150" src="/images/screenshots/h150px/gnewsense3.png" alt="gNewSense Snapshot 3"/></a>
+<p><em>gNewSense:</em></p>
+<p><?php
+# i18n: This is the description of the "gnewsense3.png" screenshot.
+print _("The system monitor showing the system resource usage."); ?></p>
 </div>
 
 </div><!-- end maincontent -->
