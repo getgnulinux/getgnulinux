47832d3df44ee888d3c0215092a6a0ab50553b32
Serrano Pereira <figure002@gmail.com>
2011-09-17 20:11:49 +0200

    Specified charset with an extra header. Specifying a character set in HTTP headers can speed up browser rendering.

--- include/header.php
+++ include/header.php
@@ -1,3 +1,4 @@
+<?php header("Content-Type: text/html; charset=utf-8"); ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php $this->text('lang'); ?>" lang="<?php $this->text('lang') ?>" dir="<?php $this->text('dir'); ?>">
 <head>
