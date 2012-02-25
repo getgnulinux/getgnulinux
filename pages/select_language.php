a85aa760f6457f705409f84d4e84da77415b9b92
Serrano Pereira <figure002@gmail.com>
2012-02-25 18:58:29 +0100

    Updated the text of the select language page. Also added an i18n comment.

--- pages/select_language.php
+++ pages/select_language.php
@@ -33,12 +33,13 @@ if (!$lang) {
 ?>
 <h2><?php print _("Check the language preferences in your browser"); ?></h2>
 <p><?php print _("You got this language selection page because your browser did not report your language preferences to the server, or reported preferences that do not contain English."); ?></p>
-<p><?php print _("If you understand English at all, you are encouraged to modify your browser's language preferences to include English. At the same time, check the preferences as a whole. You can find advice for several browsers in the <a href=\"http://www.debian.org/\">Debian</a> project <a href=\"http://www.debian.org/intro/cn\">instructions for setting language preferences</a>."); ?></p>
+<p><?php # i18n: The linked debian.org web pages are multilingual and the correct language is automatically displayed using language negotiation. So it is recommended to keep these URLs as they are.
+print _("If you understand English at all, you are encouraged to modify your browser's language preferences to include English. At the same time, check the preferences as a whole. You can find advice for several browsers in the <a href=\"http://www.debian.org/\">Debian</a> project <a href=\"http://www.debian.org/intro/cn\">instructions for setting language preferences</a>."); ?></p>
 <p><?php print _("The instructions warn that if you wish to include a particular variant of a language, such as British English (<code>en-GB</code>), you should also have that language in general as an entry (English, <code>en</code>) in the list (after the more specific entry)."); ?></p>
 <?php } ?>
 
 <h2><?php print _("More languages"); ?></h2>
-<p><?php $p = _("Our translators are working on the following languages. You can <a href=\"%s\" title=\"Translation\">help us</a> reach an audience in your language by helping out with the translations."); printf($p, $ggl->get('l10n_url')); ?></p>
+<p><?php $p = _("The translations for the following languages are currently incomplete. You can <a href=\"%s\" title=\"Translation\">help us</a> reach an audience in your language by helping out with the translations."); printf($p, $ggl->get('l10n_url')); ?></p>
 
 <div class="list_select_language three_columns">
 <ul>
