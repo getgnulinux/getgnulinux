4934bca98d3cf337438bf5517924fc340136a724
Serrano Pereira <figure002@gmail.com>
2012-02-29 01:02:53 +0100

    Removed some redundant link buttons from the link buttons page.

--- pages/link_buttons.php
+++ pages/link_buttons.php
@@ -49,24 +49,12 @@ $this->load_top_bar();
     </div>
     <div class="section">
         <div class="link_button">
-            <img src="/links/en/linuxliberated_1_100x150.png" alt="Get Linux" width="100" height="150" />
-            <textarea class="copycode" rows="5" cols="45">&lt;a href=&quot;<?php $this->text('base_url'); ?>&quot; title=&quot;Get Linux - an alternative to Windows; free as in beer and speech&quot;&gt;&lt;img src=&quot;<?php $this->text('base_url'); ?>links/en/100x150_1.png&quot; alt=&quot;Get Linux&quot; width=&quot;100&quot; height=&quot;150&quot; border=&quot;0&quot;/&gt;&lt;/a&gt;</textarea>
-        </div>
-        <div class="link_button">
             <img src="/links/en/linuxliberated_2_100x150.png" alt="Get Linux" width="100" height="150" />
             <textarea class="copycode" rows="5" cols="45">&lt;a href=&quot;<?php $this->text('base_url'); ?>&quot; title=&quot;Get Linux - an alternative to Windows; free as in beer and speech&quot;&gt;&lt;img src=&quot;<?php $this->text('base_url'); ?>links/en/100x150_2.png&quot; alt=&quot;Get Linux&quot; width=&quot;100&quot; height=&quot;150&quot; border=&quot;0&quot;/&gt;&lt;/a&gt;</textarea>
         </div>
         <div class="link_button">
-            <img src="/links/en/linuxliberated_3_100x150.png" alt="Get Linux" width="100" height="150" />
-            <textarea class="copycode" rows="5" cols="45">&lt;a href=&quot;<?php $this->text('base_url'); ?>&quot; title=&quot;Get Linux - an alternative to Windows; free as in beer and speech&quot;&gt;&lt;img src=&quot;<?php $this->text('base_url'); ?>links/en/100x150_3.png&quot; alt=&quot;Get Linux&quot; width=&quot;100&quot; height=&quot;150&quot; border=&quot;0&quot;/&gt;&lt;/a&gt;</textarea>
-        </div>
-        <div class="link_button">
             <img src="/links/en/linuxliberated_4_100x150.png" alt="Get Linux" width="100" height="150" />
             <textarea class="copycode" rows="5" cols="45">&lt;a href=&quot;<?php $this->text('base_url'); ?>&quot; title=&quot;Get Linux - an alternative to Windows; free as in beer and speech&quot;&gt;&lt;img src=&quot;<?php $this->text('base_url'); ?>links/en/100x150_4.png&quot; alt=&quot;Get Linux&quot; width=&quot;100&quot; height=&quot;150&quot; border=&quot;0&quot;/&gt;&lt;/a&gt;</textarea>
-        </div>
-        <div class="link_button">
-            <img src="/links/en/linuxliberated_5_100x150.png" alt="Get Linux" width="100" height="150" />
-            <textarea class="copycode" rows="5" cols="45">&lt;a href=&quot;<?php $this->text('base_url'); ?>&quot; title=&quot;Get Linux - an alternative to Windows; free as in beer and speech&quot;&gt;&lt;img src=&quot;<?php $this->text('base_url'); ?>links/en/100x150_5.png&quot; alt=&quot;Get Linux&quot; width=&quot;100&quot; height=&quot;150&quot; border=&quot;0&quot;/&gt;&lt;/a&gt;</textarea>
         </div>
         <div class="link_button">
             <img src="/links/en/linuxliberated_6_100x150.png" alt="Get Linux" width="100" height="150" />
@@ -75,24 +63,12 @@ $this->load_top_bar();
     </div>
     <div class="section">
         <div class="link_button">
-            <img src="/links/en/linuxliberated_1_78x116.png" alt="Linux" width="78" height="116" />
-            <textarea class="copycode" rows="5" cols="45">&lt;a href=&quot;<?php $this->text('base_url'); ?>&quot; title=&quot;Linux - an alternative to Windows; free as in beer and speech&quot;&gt;&lt;img src=&quot;<?php $this->text('base_url'); ?>links/en/78x116_1.png&quot; alt=&quot;Linux&quot; width=&quot;78&quot; height=&quot;116&quot; border=&quot;0&quot;/&gt;&lt;/a&gt;</textarea>
-        </div>
-        <div class="link_button">
             <img src="/links/en/linuxliberated_2_78x116.png" alt="Linux" width="78" height="116" />
             <textarea class="copycode" rows="5" cols="45">&lt;a href=&quot;<?php $this->text('base_url'); ?>&quot; title=&quot;Linux - an alternative to Windows; free as in beer and speech&quot;&gt;&lt;img src=&quot;<?php $this->text('base_url'); ?>links/en/78x116_2.png&quot; alt=&quot;Linux&quot; width=&quot;78&quot; height=&quot;116&quot; border=&quot;0&quot;/&gt;&lt;/a&gt;</textarea>
         </div>
         <div class="link_button">
-            <img src="/links/en/linuxliberated_3_78x116.png" alt="Linux" width="78" height="116" />
-            <textarea class="copycode" rows="5" cols="45">&lt;a href=&quot;<?php $this->text('base_url'); ?>&quot; title=&quot;Get Linux - an alternative to Windows; free as in beer and speech&quot;&gt;&lt;img src=&quot;<?php $this->text('base_url'); ?>links/en/78x116_3.png&quot; alt=&quot;Linux&quot; width=&quot;78&quot; height=&quot;116&quot; border=&quot;0&quot;/&gt;&lt;/a&gt;</textarea>
-        </div>
-        <div class="link_button">
             <img src="/links/en/linuxliberated_4_78x116.png" alt="Linux" width="78" height="116" />
             <textarea class="copycode" rows="5" cols="45">&lt;a href=&quot;<?php $this->text('base_url'); ?>&quot; title=&quot;Linux - an alternative to Windows; free as in beer and speech&quot;&gt;&lt;img src=&quot;<?php $this->text('base_url'); ?>links/en/78x116_4.png&quot; alt=&quot;Linux&quot; width=&quot;78&quot; height=&quot;116&quot; border=&quot;0&quot;/&gt;&lt;/a&gt;</textarea>
-        </div>
-        <div class="link_button">
-            <img src="/links/en/linuxliberated_5_78x116.png" alt="Linux" width="78" height="116" />
-            <textarea class="copycode" rows="5" cols="45">&lt;a href=&quot;<?php $this->text('base_url'); ?>&quot; title=&quot;Linux - an alternative to Windows; free as in beer and speech&quot;&gt;&lt;img src=&quot;<?php $this->text('base_url'); ?>links/en/78x116_5.png&quot; alt=&quot;Linux&quot; width=&quot;78&quot; height=&quot;116&quot; border=&quot;0&quot;/&gt;&lt;/a&gt;</textarea>
         </div>
         <div class="link_button">
             <img src="/links/en/linuxliberated_6_78x116.png" alt="Linux" width="78" height="116" />
