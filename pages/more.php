e3e16431de3e7f546b622ece06f746248a12b14e
Serrano Pereira <serrano.pereira@gmail.com>
2014-02-16 12:14:00 +0100

    Added translator comments to some translatable strings on the More page

--- pages/more.php
+++ pages/more.php
@@ -30,7 +30,8 @@ $this->load_header();
             print _("LinuxQuestions.org"); ?>"/></a>
           </div>
 
-          <p><?php print _("Friendly free software support forums, for newcomers and advanced users alike.<br/> If you have a question that we haven't covered, it's a good place to ask!"); ?></p>
+          <p><?php # translators: This is the text below a button linking to www.linuxquestions.org. If you translated the URL, change this text accordingly.
+          print _("Friendly free software support forums, for newcomers and advanced users alike.<br/> If you have a question that we haven't covered, it's a good place to ask!"); ?></p>
         </div>
 
         <div class="column">
@@ -41,7 +42,8 @@ $this->load_header();
             print _("Linux Pre-Loaded"); ?>"/></a>
           </div>
 
-          <p><?php print _("A simple and clear selection of vendors selling computers pre-installed with Linux."); ?></p>
+          <p><?php # translators: This is the text below a button linking to linuxpreloaded.com. If you translated the URL, change this text accordingly.
+          print _("A simple and clear selection of vendors selling computers pre-installed with Linux."); ?></p>
         </div>
       </div>
 
@@ -56,7 +58,8 @@ $this->load_header();
             print _("FLOSS Booklet"); ?>"/></a>
           </div>
 
-          <p><?php print _("A short, entertaining way to clarify thoughts about free, libre, \"open-source\" software."); ?></p>
+          <p><?php # translators: This is the text below a button linking to en.wikibooks.org/wiki/FLOSS_Concept_Booklet. If you translated the URL, change this text accordingly.
+          print _("A short, entertaining way to clarify thoughts about free, libre, \"open-source\" software."); ?></p>
         </div>
         <div class="column">
           <div>
@@ -66,7 +69,8 @@ $this->load_header();
             print _("FSF"); ?>"/></a>
           </div>
 
-          <p><?php print _("The Free Software Foundation defends and advocates the very concept of <a href=\"http://www.softwareliberty.com/\">free software</a> that gave birth to GNU/Linux."); ?></p>
+          <p><?php # translators: This is the text below a button linking to www.fsf.org. If you translated the URL, change this text accordingly.
+          print _("The Free Software Foundation defends and advocates the very concept of <a href=\"http://www.softwareliberty.com/\">free software</a> that gave birth to GNU/Linux."); ?></p>
         </div>
       </div>
 
