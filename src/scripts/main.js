7568031f5dca60fce937310b08cbc6a2ec6f83a6
Serrano Pereira <serrano@bitosis.nl>
2017-06-25 23:44:49 +0200

    Fix some linting errors

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -67,15 +67,15 @@ $(document).ready(function() {
     });
   }());
 
-  $(window).on("load resize",function(e) {
-    var menuToggle = $("#js-navigation-mobile-menu").unbind();
-    $("#js-navigation-menu").removeClass("show");
+  $(window).on('load resize',function(e) {
+    var menuToggle = $('#js-navigation-mobile-menu').unbind();
+    $('#js-navigation-menu').removeClass('show');
 
-    menuToggle.on("click", function(e) {
+    menuToggle.on('click', function(e) {
       e.preventDefault();
-      $("#js-navigation-menu").slideToggle(function(){
-        if($("#js-navigation-menu").is(":hidden")) {
-          $("#js-navigation-menu").removeAttr("style");
+      $('#js-navigation-menu').slideToggle(function(){
+        if($('#js-navigation-menu').is(':hidden')) {
+          $('#js-navigation-menu').removeAttr('style');
         }
       });
     });
