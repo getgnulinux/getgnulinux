76e9c0036ed81916a66e81052925bfbc34b2a585
Serrano Pereira <serrano@bitosis.nl>
2017-07-26 21:38:59 +0200

    Update instructions on the setup page
    Just refer to the README, as I don't like to duplicate instructions.

--- src/templates/setup.php
+++ src/templates/setup.php
@@ -11,15 +11,7 @@
   <div class="container">
     <section>
       <h1>get GNU/Linux!</h1>
-      <p>Almost there! Please follow these steps.</p>
-      <ol>
-        <li>Open a terminal and change to the root folder;</li>
-        <li>Run `<code>make settings.php</code>' to create the settings.php file (or create a copy of <code>settings.php.new</code>);</li>
-        <li>Open settings.php in a text editor and change the settings where necessary;</li>
-        <li>Run `<code>make</code>' to generate the binary locale files (requires `gettext');</li>
-        <li>Make sure that Apache module <code>mod_rewrite</code> is enabled;</li>
-        <li>To make translations work, you need to generate locale definition files on the server. On commercial web hosting servers, there is no need to do this manually. If you are on a Debian based system, you may execute `<code>scripts/make-locales.sh</code>' as root to generate them.</li>
-      </ol>
+      <p>Almost there! Please follow the instructions in the README to complete the installation.</p>
       <p>Finally, reload this page.</p>
     </section>
   </div>
