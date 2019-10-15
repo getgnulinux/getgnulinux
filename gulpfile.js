3aa8be0d2515a97d2191620f5df0ed48e6e6a69c
Serrano Pereira <serrano@bitosis.nl>
2019-10-15 20:02:54 +0200

    Enforce consistent styling for Javascript scripts
    The gulpfile.js and package.json were cleaned up in the process.

--- gulpfile.js
+++ gulpfile.js
@@ -1,179 +1,167 @@
 const gulp = require('gulp');
 const gulpLoadPlugins = require('gulp-load-plugins');
 const del = require('del');
-
 const $ = gulpLoadPlugins();
 
 const sassConfig = {
-  outputStyle: 'expanded',
-  precision: 10,
-  includePaths: [
-    'bower_components/normalize-css/',
-    'bower_components/bourbon/app/assets/stylesheets/',
-    'bower_components/neat/app/assets/stylesheets',
-  ]
-}
+    outputStyle: 'expanded',
+    precision: 10,
+    includePaths: [
+        'bower_components/normalize-css/',
+        'bower_components/bourbon/app/assets/stylesheets/',
+        'bower_components/neat/app/assets/stylesheets',
+    ]
+};
 
 const versionConfig = {
-  'value': '%MDS%',
-  'append': {
-    'key': 'v',
-    'to': [
-      'css',
-      'js',
-    ],
-  },
+    'value': '%MDS%',
+    'append': {
+        'key': 'v',
+        'to': [
+            'css',
+            'js',
+        ],
+    },
 };
 
 // Rsync configuration for deploying to development.
 const rsyncConfDev = {
-  root: 'docroot/',
-  destination: '/var/www/html',
-  progress: false,
-  incremental: true,
-  relative: true,
-  recursive: true,
-  clean: true,
-  compress: false,
-  exclude: ['.*.swp'],
+    root: 'docroot/',
+    destination: '/var/www/html',
+    progress: false,
+    incremental: true,
+    relative: true,
+    recursive: true,
+    clean: true,
+    compress: false,
+    exclude: ['.*.swp'],
 };
 
 // Rsync configuration for deploying to production.
 const rsyncConfProd = {
-  root: 'docroot/',
-  hostname: 'tuxfamily',
-  destination: '/home/getgnulinux/getgnulinux.org-web/htdocs',
-  progress: true,
-  incremental: true,
-  relative: true,
-  recursive: true,
-  clean: true,
-  compress: true,
-  exclude: ['.*.swp'],
+    root: 'docroot/',
+    hostname: 'tuxfamily',
+    destination: '/home/getgnulinux/getgnulinux.org-web/htdocs',
+    progress: true,
+    incremental: true,
+    relative: true,
+    recursive: true,
+    clean: true,
+    compress: true,
+    exclude: ['.*.swp'],
 };
 
-let dev = true;
+const lint = (files, options = {fix: false}) => {
+    return gulp.src(files)
+        .pipe($.eslint(options))
+        .pipe($.eslint.format());
+};
 
 gulp.task('styles:sass', () => {
-  return gulp.src('src/styles/*.scss')
-    .pipe($.plumber())
-    .pipe($.sourcemaps.init())
-    .pipe($.sass.sync(sassConfig).on('error', $.sass.logError))
-    .pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
-    .pipe($.cssnano({safe: true, autoprefixer: false}))
-    .pipe($.sourcemaps.write('.'), {includeContent: true})
-    .pipe(gulp.dest('docroot/styles'));
+    return gulp.src('src/styles/*.scss')
+        .pipe($.plumber())
+        .pipe($.sourcemaps.init())
+        .pipe($.sass.sync(sassConfig).on('error', $.sass.logError))
+        .pipe($.autoprefixer({browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']}))
+        .pipe($.cssnano({safe: true, autoprefixer: false}))
+        .pipe($.sourcemaps.write('.'), {includeContent: true})
+        .pipe(gulp.dest('docroot/styles'));
 });
 
 gulp.task('styles:vendor', () => {
-  return gulp.src([
-      'bower_components/flexslider/flexslider.css',
+    return gulp.src([
+        'bower_components/flexslider/flexslider.css',
     ])
-    .pipe($.cssnano({safe: true, autoprefixer: false}))
-    .pipe(gulp.dest('docroot/styles/vendor'));
+        .pipe($.cssnano({safe: true, autoprefixer: false}))
+        .pipe(gulp.dest('docroot/styles/vendor'));
 });
 
 gulp.task('styles', gulp.series('styles:sass', 'styles:vendor'));
 
 gulp.task('scripts:main', () => {
-  return gulp.src('src/scripts/main.js')
-    .pipe($.plumber())
-    .pipe($.sourcemaps.init())
-    .pipe($.babel())
-    .pipe($.uglify())
-    .pipe($.sourcemaps.write('.'), {includeContent: true})
-    .pipe(gulp.dest('docroot/scripts'));
+    return gulp.src('src/scripts/main.js')
+        .pipe($.plumber())
+        .pipe($.sourcemaps.init())
+        .pipe($.babel())
+        .pipe($.uglify())
+        .pipe($.sourcemaps.write('.'), {includeContent: true})
+        .pipe(gulp.dest('docroot/scripts'));
 });
 
 gulp.task('scripts:plugins', () => {
-  return gulp.src([
-      'src/scripts/plugins.js',
-      'bower_components/flexslider/jquery.flexslider-min.js',
+    return gulp.src([
+        'src/scripts/plugins.js',
+        'bower_components/flexslider/jquery.flexslider-min.js',
     ])
-    .pipe($.concat('plugins.js'))
-    .pipe(gulp.dest('docroot/scripts'));
+        .pipe($.concat('plugins.js'))
+        .pipe(gulp.dest('docroot/scripts'));
 });
 
 gulp.task('scripts', gulp.series('scripts:main', 'scripts:plugins'));
 
 gulp.task('fonts', () => {
-  return gulp.src(
-      require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}', function (err) {})
-    .concat('bower_components/flexslider/fonts/*'))
-    .pipe(gulp.dest('docroot/styles/vendor/fonts'));
+    return gulp.src(
+        require('main-bower-files')('**/*.{eot,svg,ttf,woff,woff2}')
+            .concat('bower_components/flexslider/fonts/*')
+    )
+        .pipe(gulp.dest('docroot/styles/vendor/fonts'));
 });
 
-function lint(files, options) {
-  return gulp.src(files)
-    .pipe($.eslint({ fix: false }))
-    .pipe($.eslint.format());
-}
+gulp.task('lint:gulpfile', () => lint(['gulpfile.js']));
 
-gulp.task('lint', () => {
-  return lint(['src/scripts/main.js'])
-    .pipe(gulp.dest('src/scripts'));
+gulp.task('lint:src', () => {
+    return lint(['src/scripts/main.js']).pipe(gulp.dest('src/scripts'));
 });
+
+gulp.task('lint', gulp.series('lint:gulpfile', 'lint:src'));
 
 gulp.task('html', gulp.series('styles', 'scripts', () => {
-  return gulp.src('src/**/*.php')
-    .pipe($.useref({searchPath: ['docroot', 'src', '.']}))
-    .pipe($.if('*.php', $.versionNumber(versionConfig)))
-    .pipe(gulp.dest('docroot'));
+    return gulp.src('src/**/*.php')
+        .pipe($.useref({searchPath: ['docroot', 'src', '.']}))
+        .pipe($.if('*.php', $.versionNumber(versionConfig)))
+        .pipe(gulp.dest('docroot'));
 }));
 
 gulp.task('images', () => {
-  return gulp.src([
-      'src/images/**/*',
-      '!src/images/src/**/*',
+    return gulp.src([
+        'src/images/**/*',
+        '!src/images/src/**/*',
     ])
-    .pipe($.cache($.imagemin()))
-    .pipe(gulp.dest('docroot/images'));
+        .pipe($.cache($.imagemin()))
+        .pipe(gulp.dest('docroot/images'));
 });
 
 gulp.task('clean', del.bind(null, [
-  'docroot/scripts',
-  'docroot/styles',
-  'docroot/templates',
+    'docroot/scripts',
+    'docroot/styles',
+    'docroot/templates',
 ]));
 
 gulp.task('clean:images', del.bind(null, [
-  'docroot/images',
+    'docroot/images',
 ]));
 
-gulp.task('watch', () => gulp.series('clean', 'html', 'fonts', () => {
-    gulp.watch('src/templates/*.php', ['html']);
-    gulp.watch('src/styles/*.scss', ['styles', 'html']);
-    gulp.watch('src/scripts/*.js', ['scripts', 'html']);
-    gulp.watch('src/styles/vendor/fonts/*', ['fonts']);
-}));
-
 gulp.task('build', gulp.series('lint', 'html', 'images', 'fonts', () => {
-  return gulp.src('src/**/*')
-    .pipe($.size({title: 'build', gzip: true}));
+    return gulp.src('src/**/*').pipe($.size({title: 'build', gzip: true}));
 }));
 
-gulp.task('default', () => {
-  return new Promise(resolve => {
-    dev = false;
-    gulp.series('clean', 'build', resolve);
-  });
-});
+gulp.task('default', gulp.series('clean', 'build'));
 
 gulp.task('deploy:dev', () => {
-  const conf = rsyncConfDev;
-  return gulp.src('docroot/**').pipe($.rsync(conf));
+    return gulp.src('docroot/**').pipe($.rsync(rsyncConfDev));
 });
 
-gulp.task('develop', gulp.series('clean', 'html', 'fonts', 'deploy:dev'));
+gulp.task('develop', gulp.series('clean', 'build', 'deploy:dev'));
 
 gulp.task('deploy:prod', () => {
-  const conf = rsyncConfProd;
-  return gulp.src('docroot/**')
-	.pipe(
-	  $.prompt.confirm({
-		message: `Are you sure you want to deploy to ${conf.hostname}:${conf.destination}?`,
-		default: false
-	  })
-	)
-	.pipe($.rsync(conf));
+    const conf = rsyncConfProd;
+
+    return gulp.src('docroot/**')
+        .pipe(
+            $.prompt.confirm({
+                message: `Are you sure you want to deploy to ${conf.hostname}:${conf.destination}?`,
+                default: false
+            })
+        )
+        .pipe($.rsync(conf));
 });
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,68 +1,68 @@
 $(document).ready(function() {
-  /* Enable PNG fallback for browsers not supporting SVG in img tags */
-  svgeezy.init(false, 'png');
+    /* Enable PNG fallback for browsers not supporting SVG in img tags */
+    svgeezy.init(false, 'png');
 
-  /* Show an alert box if the browser is outdated */
-  (function() {
-    var $box = $('.alertbox.browsehappy'),
-        stateCookieName = 'alertbox_outdated_browser',
-        alreadyClosed = $.cookie(stateCookieName);
+    /* Show an alert box if the browser is outdated */
+    (function() {
+        var $box = $('.alertbox.browsehappy'),
+            stateCookieName = 'alertbox_outdated_browser',
+            alreadyClosed = $.cookie(stateCookieName);
 
-    /* Use absence of SVG support as a criterion for an outdated browser */
-    if (!Modernizr.svg && !alreadyClosed) {
-      $box.removeClass('hidden');
-    }
-
-    $box.find('.close').click(function() {
-      $box.slideUp();
-      $.cookie(stateCookieName, 1, { expires: 30, path: '/' });
-    });
-  })();
-
-  (function() {
-    var settings = {
-      text: 'To Top',
-      min: 200,
-      inDelay: 600,
-      outDelay: 400,
-      containerID: 'to-top',
-      containerHoverID: 'to-top-hover',
-      scrollSpeed: 400,
-      easingType: 'linear'
-    };
-
-    var toTopHidden = true;
-    var toTop = $('#' + settings.containerID);
-
-    toTop.click(function(e) {
-      e.preventDefault();
-      $.scrollTo(0, settings.scrollSpeed, {easing: settings.easingType});
-    });
-
-    $(window).scroll(function() {
-      var sd = $(this).scrollTop();
-      if (sd > settings.min && toTopHidden) {
-        toTop.fadeIn(settings.inDelay);
-        toTopHidden = false;
-      }
-      else if (sd <= settings.min && ! toTopHidden) {
-        toTop.fadeOut(settings.outDelay);
-        toTopHidden = true;
-      }
-    });
-  }());
-
-  $(window).on('load resize',function(e) {
-    var menuToggle = $('#js-navigation-mobile-menu').unbind();
-    $('#js-navigation-menu').removeClass('show');
-
-    menuToggle.on('click', function(e) {
-      e.preventDefault();
-      $('#js-navigation-menu').slideToggle(function(){
-        if($('#js-navigation-menu').is(':hidden')) {
-          $('#js-navigation-menu').removeAttr('style');
+        /* Use absence of SVG support as a criterion for an outdated browser */
+        if (!Modernizr.svg && !alreadyClosed) {
+            $box.removeClass('hidden');
         }
-      });
+
+        $box.find('.close').click(function() {
+            $box.slideUp();
+            $.cookie(stateCookieName, 1, { expires: 30, path: '/' });
+        });
+    })();
+
+    (function() {
+        var settings = {
+            text: 'To Top',
+            min: 200,
+            inDelay: 600,
+            outDelay: 400,
+            containerID: 'to-top',
+            containerHoverID: 'to-top-hover',
+            scrollSpeed: 400,
+            easingType: 'linear'
+        };
+
+        var toTopHidden = true;
+        var toTop = $('#' + settings.containerID);
+
+        toTop.click(function(e) {
+            e.preventDefault();
+            $.scrollTo(0, settings.scrollSpeed, {easing: settings.easingType});
+        });
+
+        $(window).scroll(function() {
+            var sd = $(this).scrollTop();
+            if (sd > settings.min && toTopHidden) {
+                toTop.fadeIn(settings.inDelay);
+                toTopHidden = false;
+            }
+            else if (sd <= settings.min && ! toTopHidden) {
+                toTop.fadeOut(settings.outDelay);
+                toTopHidden = true;
+            }
+        });
+    }());
+
+    $(window).on('load resize', function() {
+        var menuToggle = $('#js-navigation-mobile-menu').unbind();
+        $('#js-navigation-menu').removeClass('show');
+
+        menuToggle.on('click', function(e) {
+            e.preventDefault();
+            $('#js-navigation-menu').slideToggle(function(){
+                if($('#js-navigation-menu').is(':hidden')) {
+                    $('#js-navigation-menu').removeAttr('style');
+                }
+            });
+        });
     });
-  });
 });
