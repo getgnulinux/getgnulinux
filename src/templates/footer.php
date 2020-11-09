2fd8e83bccdd770fc6bac6bb887b9fe952903396
丁乾坤 <k737566@gmail.com>
2020-11-09 13:11:12 +0800

    fix description in footer

--- src/templates/footer.php
+++ src/templates/footer.php
@@ -45,7 +45,7 @@
         <?php # translators: If there is an official translation for the license, use that URL instead. Make sure you use the URL for CC BY-SA 4.0.
         $p = _("Except where otherwise <a href=\"%s\">noted</a>, content on this site is licensed under a <a href=\"https://creativecommons.org/licenses/by-sa/4.0/\" rel=\"license external\"><abbr title=\"Creative Commons Attribution-ShareAlike\">CC BY-SA</abbr> 4.0 International License</a>."); printf($p, $this->get_base_url('legal')); ?><br/>
         <?php print _("Generously hosted by <a href=\"https://www.tuxfamily.org/\" rel=\"external\">TuxFamily</a>."); ?>
-        <?php $p = _("This site is <a href=\"%s\" rel=\"external\">open source</a>."); printf($p, $ggl->get('project_url')); ?>
+        <?php $p = _("This site is <a href=\"%s\" rel=\"external\">free and open source</a>."); printf($p, $ggl->get('project_url')); ?>
       </p>
     </div>
 
