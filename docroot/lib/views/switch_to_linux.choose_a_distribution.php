50abda617a0c0dabe8861053c192b1f9df43eed3
Serrano Pereira <serrano@bitosis.nl>
2020-11-26 21:00:40 +0100

    Replace Trisquel by Debian
    Not endorsed by GNU, but it is one of the most popular and widely used
    distributions.
    
    Debian was moved to the top of the recommended list as well and the
    distribution descriptions were updated.

--- docroot/lib/views/switch_to_linux.choose_a_distribution.php
+++ docroot/lib/views/switch_to_linux.choose_a_distribution.php
@@ -6,6 +6,13 @@ if ( !defined('GGL') ) {
 
 $this->load_header();
 
+# translators: This is the description of the "debian1.jpg" screenshot.
+$debian1_text = _("Accessing applications in the <a href=\"https://www.gnome.org/gnome-3/\" rel=\"external\">GNOME 3</a> desktop environment.");
+# translators: This is the description of the "debian2.jpg" screenshot.
+$debian2_text = _("LibreOffice Writer open, editing a standard document.");
+# translators: This is the description of the "debian3.jpg" screenshot.
+$debian3_text = _("Shotwell photo manager");
+
 # translators: This is the description of the "ubuntu1.jpg" screenshot.
 $ubuntu1_text = _("Firefox web browser");
 # translators: This is the description of the "ubuntu2.jpg" screenshot.
@@ -19,13 +26,6 @@ $fedora1_text = _("Accessing applications in the <a href=\"https://www.gnome.org
 $fedora2_text = _("Desktop overview with two applications open.");
 # translators: This is the description of the "fedora3.jpg" screenshot.
 $fedora3_text = _("Software manager open, install/remove applications.");
-
-# translators: This is the description of the "trisquel1.jpg" screenshot.
-$trisquel1_text = _("Accessing applications.");
-# translators: This is the description of the "trisquel2.jpg" screenshot.
-$trisquel2_text = _("LibreOffice Writer open, editing a standard document.");
-# translators: This is the description of the "trisquel3.jpg" screenshot.
-$trisquel3_text = _("LibreOffice Calc open, showing an example spreadsheet.");
 ?>
 
 <nav class="chapters">
@@ -46,15 +46,42 @@ $trisquel3_text = _("LibreOffice Calc open, showing an example spreadsheet.");
       <?php print _("This is our selection for newcomers. These links are not commercial."); ?></p>
     </header>
 
+    <section class="debian">
+      <div class="container">
+        <div class="centered">
+          <img src="/images/DebianLogo.png" class="distro-logo" alt="Debian">
+        </div>
+
+        <p><?php $p = _("Debian is one of the oldest operating systems based on the Linux kernel. It has the reputation of being a very stable and secure GNU/Linux distribution which is backed by a well-organized community of developers and users. For this reason it is popular for use on personal computers as well as servers. Debian is also the basis for many other distributions, most notably Ubuntu."); printf($p, $this->get_base_url('linux/linux_faq')); ?></p>
+        <p><?php print _("We recommend this distribution if you are committed to freedom in your computing."); ?></p>
+
+        <div class="flexslider flexslider_screenshots">
+          <ul class="slides">
+            <li>
+              <img src="/images/screenshots/debian1.jpg" alt="Debian screenshot" />
+              <p class="flex-caption"><?php print $debian1_text; ?></p>
+            </li>
+            <li>
+              <img src="/images/screenshots/debian2.jpg" alt="Debian screenshot" />
+              <p class="flex-caption"><?php print $debian2_text; ?></p>
+            </li>
+            <li>
+              <img src="/images/screenshots/debian3.jpg" alt="Debian screenshot" />
+              <p class="flex-caption"><?php print $debian3_text; ?></p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+
     <section class="ubuntu">
       <div class="container">
         <div class="centered">
           <img src="/images/UbuntuLogo.png" class="distro-logo" alt="Ubuntu">
         </div>
 
-        <p><?php print _("Simple, easy, usable and our personal favourite. A tanned, smooth atmosphere that never gets in your way. All programs are kept in order and managed easily. Configuration is kept simple rather than exhaustive."); ?></p>
+        <p><?php print _("Ubuntu is a GNU/Linux distribution based on Debian. The philosophy behind Ubuntu is that every computer user should be able to download and use their software free of charge, that anyone should be able to use their software in the language of their choice, and that anyone should be able to use all software regardless of disability. The result is an operating system that has excellent hardware support, is easy to use and pleasant on the eyes."); ?></p>
         <p><?php print _("We recommend this friendly and complete distribution if you are looking for an easy way to try and step into GNU/Linux."); ?></p>
-        <p><?php print _("Ubuntu also has a sister distribution, <a href=\"https://www.kubuntu.org/\" rel=\"external\">Kubuntu</a>, with a different layout and slightly more advanced graphics."); ?></p>
 
         <div class="flexslider flexslider_screenshots">
           <ul class="slides">
@@ -81,9 +108,7 @@ $trisquel3_text = _("LibreOffice Calc open, showing an example spreadsheet.");
           <img src="/images/FedoraLogo.png" class="distro-logo" alt="Fedora">
         </div>
 
-        <p><?php
-        # translators: if you don't know of a clean, simple comparison website, simply remove the link.
-        print _("The distribution by the long-time prominent GNU/Linux developer company Red Hat. The default layout is similar to Ubuntu (it is also possible to use KDE, the more advanced desktop environment in use in Kubuntu), but there are differences in the chosen applications, install protocols and several technical elements."); ?></p>
+        <p><?php print _("The distribution by the long-time prominent GNU/Linux developer company Red Hat. The default layout is similar to Debian, but there are differences in the chosen applications, install protocols and several technical elements. The Fedora developers are committed to innovation, which is reflected by incorporating new technologies early on, providing the latest in stable and robust, useful, and powerful free software."); ?></p>
         <p><?php print _("We recommend this distribution if you wish to learn more about the workings of a GNU/Linux system, security configuration, or work on software development."); ?></p>
 
         <div class="flexslider flexslider_screenshots">
@@ -100,34 +125,6 @@ $trisquel3_text = _("LibreOffice Calc open, showing an example spreadsheet.");
               <img src="/images/screenshots/fedora3.jpg" alt="Fedora screenshot" />
               <p class="flex-caption"><?php print $fedora3_text; ?></p>
            </li>
-          </ul>
-        </div>
-      </div>
-    </section>
-
-    <section class="trisquel">
-      <div class="container">
-        <div class="centered">
-          <img src="/images/TrisquelLogo.png" class="distro-logo" alt="Trisquel">
-        </div>
-
-        <p><?php $p = _("Since Ubuntu contains some restricting components (<a href=\"%s#full_freedom\">why?</a>), the Trisquel developers have built an entirely free derivative. Although its community is smaller and its hardware support might be slightly inferior, Trisquel gives anyone full freedom over a complete operating system similar to Ubuntu."); printf($p, $this->get_base_url('linux/linux_faq')); ?></p>
-        <p><?php print _("We recommend this distribution if you are committed to freedom in your computing."); ?></p>
-
-        <div class="flexslider flexslider_screenshots">
-          <ul class="slides">
-            <li>
-              <img src="/images/screenshots/trisquel1.jpg" alt="Trisquel screenshot" />
-              <p class="flex-caption"><?php print $trisquel1_text; ?></p>
-            </li>
-            <li>
-              <img src="/images/screenshots/trisquel2.jpg" alt="Trisquel screenshot" />
-              <p class="flex-caption"><?php print $trisquel2_text; ?></p>
-            </li>
-            <li>
-              <img src="/images/screenshots/trisquel3.jpg" alt="Trisquel screenshot" />
-              <p class="flex-caption"><?php print $trisquel3_text; ?></p>
-            </li>
           </ul>
         </div>
       </div>
