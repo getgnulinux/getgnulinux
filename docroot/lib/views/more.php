b5f915cead8e96c52f23ef8e2aafa9d367bad68b
Serrano Pereira <serrano@bitosis.nl>
2020-12-12 17:18:22 +0100

    Cleanup styles for More page

--- docroot/lib/views/more.php
+++ docroot/lib/views/more.php
@@ -19,31 +19,56 @@ $this->load_header();
         </div>
       </header>
 
-      <div class="more-section">
-        <h2><?php print _("Questions and help"); ?></h2>
+      <h2 class="center"><?php print _("Questions and help"); ?></h2>
 
-        <div class="column">
-          <a href="https://unix.stackexchange.com/" title="Unix &amp; Linux Stack Exchange" target="_blank"><img src="/images/more/unix.stackexchange.png" alt="Unix &amp; Linux Stack Exchange"></a>
+      <br>
+
+      <div class="row">
+        <div class="col s12 l6 pad3">
+          <div class="center">
+            <a href="https://unix.stackexchange.com/" title="Unix &amp; Linux Stack Exchange" target="_blank">
+              <img class="lift-effect" src="/images/more/unix.stackexchange.png" alt="Unix &amp; Linux Stack Exchange">
+            </a>
+          </div>
           <p><?php print _("<strong>Unix &amp; Linux Stack Exchange</strong> is a question and answer site for users of GNU/Linux. This site is all about getting answers."); ?></p>
-          <br>
-          <a href="https://ask.fedoraproject.org/" title="Ask Fedora" target="_blank"><img src="/images/more/askfedora.png" alt="Ask Fedora"/></a>
-          <p><?php print _("<strong>Ask Fedora</strong> is a forum for Fedora users. Here you can speak to the community to get help with installing, using, customizing, and upgrading a system running Fedora."); ?></p>
         </div>
-        <div class="column">
-          <a href="https://askubuntu.com/" title="Ask Ubuntu" target="_blank"><img src="/images/more/askubuntu.png" alt="Ask Ubuntu"/></a>
+        <div class="col s12 l6 pad3">
+          <div class="center">
+            <a href="https://askubuntu.com/" title="Ask Ubuntu" target="_blank">
+              <img class="lift-effect" src="/images/more/askubuntu.png" alt="Ask Ubuntu"/>
+            </a>
+          </div>
           <p><?php print _("<strong>Ask Ubuntu</strong> is a question and answer site for Ubuntu users. Even for users of different distributions of GNU/Linux this can be a helpful resource for finding answers."); ?></p>
+        </div>
+        <div class="col s12 l6 pad3">
+          <div class="center">
+            <a href="https://ask.fedoraproject.org/" title="Ask Fedora" target="_blank">
+              <img class="lift-effect" src="/images/more/askfedora.png" alt="Ask Fedora"/>
+            </a>
+          </div>
+          <p><?php print _("<strong>Ask Fedora</strong> is a forum for Fedora users. Here you can speak to the community to get help with installing, using, customizing, and upgrading a system running Fedora."); ?></p>
         </div>
       </div>
 
-      <div class="more-section">
-        <h2><?php print _("Learn and read more"); ?></h2>
+      <h2 class="center"><?php print _("Learn and read more"); ?></h2>
 
-        <div class="column">
-          <a href="https://en.wikibooks.org/wiki/FLOSS_Concept_Booklet" title="The FLOSS concept booklet" target="_blank"><img src="/images/more/floss-booklet.png" alt="FLOSS Booklet"></a>
+      <br>
+
+      <div class="row">
+        <div class="col s12 l6 pad3">
+          <div class="center">
+            <a href="https://en.wikibooks.org/wiki/FLOSS_Concept_Booklet" title="The FLOSS concept booklet" target="_blank">
+              <img class="lift-effect" src="/images/more/floss-booklet.png" alt="FLOSS Booklet">
+            </a>
+          </div>
           <p><?php print _("A short, entertaining way to clarify thoughts about free, libre, \"open-source\" software."); ?></p>
         </div>
-        <div class="column">
-          <a href="https://www.fsf.org/" title="Free Software Foundation" target="_blank"><img src="/images/more/fsf.png" alt="FSF"></a>
+        <div class="col s12 l6 pad3">
+          <div class="center">
+            <a href="https://www.fsf.org/" title="Free Software Foundation" target="_blank">
+              <img class="lift-effect" src="/images/more/fsf.png" alt="FSF">
+            </a>
+          </div>
           <p><?php print _("The Free Software Foundation defends and advocates the very concept of <a href=\"https://www.gnu.org/philosophy/free-sw.html\" rel=\"external\">free software</a> that gave birth to GNU/Linux."); ?></p>
         </div>
       </div>
